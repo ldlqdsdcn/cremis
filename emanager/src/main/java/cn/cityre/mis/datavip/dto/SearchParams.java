0aaaa03172f863bde27357d6ad2d0248893684f1
chedongming <chedongming@cityhouse.cn>
2017-08-17 09:25:48 +0800

    添加getter和setter

--- emanager/src/main/java/cn/cityre/mis/datavip/dto/SearchParams.java
+++ emanager/src/main/java/cn/cityre/mis/datavip/dto/SearchParams.java
@@ -22,6 +22,87 @@ public class SearchParams implements Serializable {
     private String regStartTime;
     private String regEndTime;
     private String serviceStartTime;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public String getPayTel() {
+        return payTel;
+    }
+
+    public void setPayTel(String payTel) {
+        this.payTel = payTel;
+    }
+
+    public String getPayFlag() {
+        return payFlag;
+    }
+
+    public void setPayFlag(String payFlag) {
+        this.payFlag = payFlag;
+    }
+
+    public String getNewUser() {
+        return newUser;
+    }
+
+    public void setNewUser(String newUser) {
+        this.newUser = newUser;
+    }
+
+    public String getRegStartTime() {
+        return regStartTime;
+    }
+
+    public void setRegStartTime(String regStartTime) {
+        this.regStartTime = regStartTime;
+    }
+
+    public String getRegEndTime() {
+        return regEndTime;
+    }
+
+    public void setRegEndTime(String regEndTime) {
+        this.regEndTime = regEndTime;
+    }
+
+    public String getServiceStartTime() {
+        return serviceStartTime;
+    }
+
+    public void setServiceStartTime(String serviceStartTime) {
+        this.serviceStartTime = serviceStartTime;
+    }
+
+    public String getServiceEndTime() {
+        return serviceEndTime;
+    }
+
+    public void setServiceEndTime(String serviceEndTime) {
+        this.serviceEndTime = serviceEndTime;
+    }
+
+    public QueryParams getQueryParams() {
+        return queryParams;
+    }
+
+    public void setQueryParams(QueryParams queryParams) {
+        this.queryParams = queryParams;
+    }
+
     private String serviceEndTime;
     private QueryParams queryParams;
 }
