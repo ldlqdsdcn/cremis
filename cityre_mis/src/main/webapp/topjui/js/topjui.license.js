97965c893b89609b64981a1214323487325d92c2
liudalei <liudalei@cityhouse.cn>
2017-09-27 17:11:36 +0800

    添加topjui类库

---
+++ cityre_mis/src/main/webapp/topjui/js/topjui.license.js
@@ -0,0 +1,9 @@
+/**
+ * 这是通过http://localhost调试系统时使用的授权文件，该文件不可删除！
+ *
+ * TopJUI前端框架由深圳易网时代信息技术有限公司（以下简称易网时代）研发，版权所有！
+ * 你可以在非商业软件中免费使用TopJUI，商业软件需通过易网时代授权方可使用TopJUI!
+ * 商业授权用户可获得最新稳定版TopJUI，同时TopJUI研发团队可免费提供相关技术咨询
+ * TopJUI前端框架官方网站：http://www.topjui.com
+ */
+var license = 'a1d802d22139375656235b1a923fa88660da74b6,58edbe99e211f8e664d1f5241de2e3ffb55955a6,1ce7d1a026fdd9d3028e04c4c59658832389ed56';
