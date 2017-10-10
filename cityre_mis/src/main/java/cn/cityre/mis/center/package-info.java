a7d8582bb353fcf6a69b1ff8c6376694b967cf88
liudalei <liudalei@cityhouse.cn>
2017-10-10 09:37:42 +0800

    清理掉用不到的代码，报名不合理的代码 cityre_center 添加包备注

--- cityre_mis/src/main/java/cn/cityre/mis/center/package-info.java
+++ cityre_mis/src/main/java/cn/cityre/mis/center/package-info.java
@@ -1,5 +1,5 @@
 /**
  * Created by 刘大磊 on 2017/8/23 17:19.
- * 账户库所有操作
+ * 中心库所有操作
  */
 package cn.cityre.mis.center;
---
+++ cityre_mis/src/main/java/cn/cityre/mis/cityre_center/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by 刘大磊 on 2017/10/10 9:35.
+ * 城市中心库
+ */
+package cn.cityre.mis.cityre_center;
